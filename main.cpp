41032f42434ca1b83d1f99c456a0a52ed5991750
Kamil Holub <kahol20000@gmail.com>
2023-09-03 21:57:19 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "cases.hpp"
+#include <string>
+std::string CHARSET = "10011001";
+int main()
+{
+    std::cout<<"Hi \n"<<std::flush;
+    charTranslate(CHARSET);
+
+
+}
